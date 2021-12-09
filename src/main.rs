0bf43e4861fd5073a0e8390a8b296d6facd5dc00
Eric Caleb <caleberic@tuta.io>
2021-12-09 19:05:23 +0100

    move routes to mod

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,42 @@
-use warp::Filter;
-use redis::{AsyncCommands};
 use pretty_env_logger;
+use redis::AsyncCommands;
 
-#[macro_use] extern crate log;
+#[macro_use]
+extern crate log;
 
 mod db;
-mod routes;
+mod models;
 mod redis_cli;
+mod routes;
+mod handlers;
 
 #[tokio::main]
 async fn main() {
-
     pretty_env_logger::init();
     info!("redmob");
-    
-    let rc = redis_cli::create_client().await;
-    match rc {
-        Ok(conn) => {
-            let mut conn_cli = conn;
-            let _rs:redis::RedisResult<()> = conn_cli.set("key1", b"wow").await;
-        }
-        
-        Err(err) => {println!("err , {:?}", err)}
-    }
 
-    
-    // GET /
-    let base = warp::path::end().map(|| 
-        "Redmob!"
-    );
+    // let rc = redis_cli::create_client().await;
+    // match rc {
+    //     Ok(conn) => {
+    //         let mut conn_cli = conn;
+    //         let _rs: redis::RedisResult<()> = conn_cli.set("key1", b"wow").await;
+    //     }
 
-    // GET /status
-    let status = warp::path!("status").map(|| "OK");
+    //     Err(err) => {
+    //         println!("err , {:?}", err)
+    //     }
+    // }
 
-    let routes = warp::get().and(
-        base
-        .or(status)
-    );
-    
-    warp::serve(routes)
+    // let mc = db::create_db().await;
+
+    // match mc {
+    //     Ok(_m_cli) => {}
+    //     Err(err) => {
+    //         println!("err , {:?}", err)
+    //     }
+    // }
+
+    warp::serve(routes::routes().await)
         .run(([0, 0, 0, 0], 3030))
         .await;
 }
- 
