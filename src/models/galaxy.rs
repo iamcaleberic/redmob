dc51a9cef1b861dde6a558bc7844a9eddb8bef17
Eric Caleb <caleberic@tuta.io>
2021-12-10 18:04:24 +0100

    add oid to galaxy model

--- src/models/galaxy.rs
+++ src/models/galaxy.rs
@@ -1,14 +1,13 @@
-use serde::{Serialize, Deserialize};
-use redis::{ToRedisArgs, RedisWrite};
+use redis::{RedisWrite, ToRedisArgs};
+use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Deserialize, Serialize, Debug)]
 pub struct Galaxy {
-    // #[serde(rename = "_id")]
-    // id: mongodb::bson::oid::ObjectId,
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    pub id: Option<mongodb::bson::oid::ObjectId>,
     pub name: String,
     pub constellation: String,
 }
-
 
 impl ToRedisArgs for &Galaxy {
     fn write_redis_args<W>(&self, out: &mut W)
@@ -18,4 +17,3 @@ impl ToRedisArgs for &Galaxy {
         out.write_arg_fmt(serde_json::to_string(self).expect("Can't serialize Galaxy as string"))
     }
 }
-
