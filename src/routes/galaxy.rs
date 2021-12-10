d4e5851d1cdf1990ee8884583fdbcdaef3fbc14e
Eric Caleb <caleberic@tuta.io>
2021-12-10 18:04:02 +0100

    cargo fmt

--- src/routes/galaxy.rs
+++ src/routes/galaxy.rs
@@ -1,20 +1,18 @@
-use warp::Filter;
 use mongodb::Database;
+use warp::Filter;
 
-use crate::handlers::galaxy::{create_galaxy, get_galaxies, get_galaxy_by_name};
-use super::{with_db, json_body};
-use std::collections::HashMap;
+use super::{json_body, with_db};
+use crate::handlers::galaxy::{create_galaxy, get_galaxies, get_galaxy_by_oid};
 
+// use std::collections::HashMap;
 
 pub fn galaxies(
     db: Database,
 ) -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
     create(db.clone())
-        .or(get_by_name(db.clone()))
+        .or(get_by_oid(db.clone()))
         .or(get(db.clone()))
 }
-      
-
 
 /// GET /galaxies
 pub fn get(
@@ -23,27 +21,27 @@ pub fn get(
     warp::path!("galaxies")
         .and(warp::get())
         .and(with_db(db))
-        .and_then( get_galaxies)
+        .and_then(get_galaxies)
 }
 
-/// GET /galaxies?name=value
-pub fn get_by_name(
+/// GET /galaxies/<iod>
+pub fn get_by_oid(
     db: Database,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    warp::path!("galaxies"/ "name")
+    warp::path!("galaxies" / String)
         .and(warp::get())
-        .and(warp::query::<HashMap<String, String>>())
+        // .and(warp::query::<HashMap<String, String>>())
         .and(with_db(db))
-        .and_then(get_galaxy_by_name)
+        .and_then(get_galaxy_by_oid)
 }
 
-// POST /galaxies/  
-pub fn create(db: Database) -> impl Filter<Extract=(impl warp::Reply,), Error=warp::Rejection> + Clone {
+// POST /galaxies/
+pub fn create(
+    db: Database,
+) -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
     warp::path!("galaxies")
         .and(warp::post())
         .and(json_body())
         .and(with_db(db))
         .and_then(create_galaxy)
 }
-
-
